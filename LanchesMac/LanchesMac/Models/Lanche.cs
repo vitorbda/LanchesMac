b4e0529469c8f08a1c64be9f43490e186ee322eb
vitorbda <80553392+vitorbda@users.noreply.github.com>
2022-11-14 07:27:27 -0300

    Criadas as strings de conexão com o banco de dados

--- LanchesMac/LanchesMac/Models/Lanche.cs
+++ LanchesMac/LanchesMac/Models/Lanche.cs
@@ -5,9 +5,9 @@
         public int Id { get; set; }
         public string Nome { get; set; }
         public string DescricaoCurta { get; set; }
-        public string DescricaoDetalhada { get; set; }
+        public string? DescricaoDetalhada { get; set; }
         public decimal Preco { get; set; }
-        public string ImagemUrl { get; set; }
+        public string? ImagemUrl { get; set; }
         public string ImagemThumbnailUrl { get; set; }
         public bool IsLanchePreferido { get; set; }
         public bool EmEstoque { get; set; }

4afc4bba148e9a530ab296eaca1b67c22f2dec58
vitorbda <80553392+vitorbda@users.noreply.github.com>
2022-11-16 07:33:15 -0300

    Criação da controller de lanche + view

--- LanchesMac/LanchesMac/Program.cs
+++ LanchesMac/LanchesMac/Program.cs
@@ -1,10 +1,15 @@
 using LanchesMac.Context;
+using LanchesMac.Respositories;
+using LanchesMac.Respositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddTransient<ILancheRepository, LancheRepository>();
+builder.Services.AddTransient<ICategoriaRepository, CategoriaRepository>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();

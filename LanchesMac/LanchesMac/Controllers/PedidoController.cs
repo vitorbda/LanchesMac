4e9064923d7b184377edd985272a209268e30d2b
vitorbda <80553392+vitorbda@users.noreply.github.com>
2022-12-21 08:50:53 -0300

    Criada a view do checkout

--- LanchesMac/LanchesMac/Controllers/PedidoController.cs
+++ LanchesMac/LanchesMac/Controllers/PedidoController.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LanchesMac.Models;
+using LanchesMac.Respositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
 {
     public class PedidoController : Controller
     {
-        public IActionResult Index()
+        private readonly IPedidoRepositoriy _pedidoRepositoriy;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepositoriy pedidoRepositoriy, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepositoriy = pedidoRepositoriy;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido) 
         {
             return View();
         }

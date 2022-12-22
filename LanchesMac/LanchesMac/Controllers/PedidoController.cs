20768422fd7c43bc178eb0d12622a01a3aa7156f
vitorbda <80553392+vitorbda@users.noreply.github.com>
2022-12-22 07:52:44 -0300

    Criada a função que finaliza o pedido

--- LanchesMac/LanchesMac/Controllers/PedidoController.cs
+++ LanchesMac/LanchesMac/Controllers/PedidoController.cs
@@ -23,7 +23,46 @@ namespace LanchesMac.Controllers
         [HttpPost]
         public IActionResult Checkout(Pedido pedido) 
         {
-            return View();
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0.0m;
+
+            //Busca os itens do carrinho
+            List<CarrinhoCompraItem> items = _carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItems = items;
+
+            //Se não existirem itens, retorna um erro na ModelState
+            if (_carrinhoCompra.CarrinhoCompraItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho está vazio...");
+            }
+
+            //Percorre os itens do carrinho, pega seus valores
+            foreach(var item in items)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+            //Verifica se a ModelState está valida
+            if (ModelState.IsValid)
+            {
+                //Cria o pedido e os detalhes
+                _pedidoRepositoriy.CriarPedido(pedido);
+
+                //Define a mensagem ao cliente
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido :)";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarinhoCompraTotal();
+
+                _carrinhoCompra.LimparCarrinho();
+
+                //Retorna a view com os dados do cliente e do pedido
+                return View("~/View/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+            return View(pedido);
         }
     }
 }

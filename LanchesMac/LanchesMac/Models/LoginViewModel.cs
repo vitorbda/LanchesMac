33dae733ccbbbe3468165297b388af07c474ca78
vitorbda <80553392+vitorbda@users.noreply.github.com>
2022-12-23 08:10:14 -0300

    Criada a controller de login

---
+++ LanchesMac/LanchesMac/Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Build.Framework;
+using Xunit.Sdk;
+
+namespace LanchesMac.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o nome")]
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}

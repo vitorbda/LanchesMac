8ef68dc5e1f66bc10c5dc9e00463ef9ef929ad4c
vitorbda <80553392+vitorbda@users.noreply.github.com>
2022-12-28 08:30:27 -0300

    Criação da area de Admin e seus componentes

--- LanchesMac/LanchesMac/Program.cs
+++ LanchesMac/LanchesMac/Program.cs
@@ -63,7 +63,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
   name: "areas",
-  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+  pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
